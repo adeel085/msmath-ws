b674695e75a7ac2b1300c4eb52fcfa14d0c377d3
adeel085 <adeel.ahmed9360@gmail.com>
2024-08-19 02:15:31 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", (ws) => {
+    ws.on("message", (message) => {
+        // Broadcast the message to all connected clients
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.send("Welcome to the WebSocket server");
+});
