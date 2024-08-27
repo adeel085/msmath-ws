c89d06b6c66ef3663a68017af2196c2bd73c21b0
adeel085 <adeel.ahmed9360@gmail.com>
2024-08-27 13:07:43 +0500

    Added support for multiple schools

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const WebSocket = require("ws");
+const url = require("url");
 
 const wss = new WebSocket.Server({ port: 8080 });
 
-wss.on("connection", (ws) => {
+wss.on("connection", (ws, req) => {
+    const parameters = url.parse(req.url, true).query;
+    const schoolName = parameters.school_name;
+
+    ws.schoolName = schoolName;
+
     ws.on("message", (message) => {
         // Broadcast the message to all connected clients
         wss.clients.forEach((client) => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
+            if (
+                client !== ws &&
+                client.schoolName === ws.schoolName &&
+                client.readyState === WebSocket.OPEN
+            ) {
                 client.send(message);
             }
         });
